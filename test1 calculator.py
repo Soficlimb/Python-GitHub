19eb12f7a3cb50686541782c439c6e5a1bc24917
Soficlimb <104201505+Soficlimb@users.noreply.github.com>
2022-04-22 16:03:38 +0300

    Add files via upload

---
+++ test1 calculator.py
@@ -0,0 +1,40 @@
+
+
+a = float (input("Input the first number: "))
+
+what = input('What function? (+,-,*,/) :')
+
+b = float (input("Input the second number:"))
+
+
+if what == "+":
+    c = a + b
+
+    print ("Resalt:" + str(c))
+
+
+elif what == "-":
+     c = a - b
+
+     print ("Resalt:" + str(c))
+
+elif what == "*":
+     c = a * b
+
+     print ("Resalt:" + str(c))
+
+elif what == "/":
+     c = a / b
+
+     print ("Resalt:" + str(c))
+
+else:
+     print ("Invalid operation!")    
+
+input()   
+
+   
+
+    
+
+  
